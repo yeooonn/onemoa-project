02ce5ce4acb684601affcc17a6d953e964c23e4d
kyj980303 <rladuswjd0303@naver.com>
2022-10-02 23:46:40 +0900

    메인페이지 구현(HTML,CSS,JS)

---
+++ onemoa/js/onemoa.js
@@ -0,0 +1,39 @@
+/* 메인페이지 후기 돌아가는 코드 */
+var gall  = setInterval(galleryFun, 2000);
+var inter = true;
+var idx = 2;
+
+function galleryFun(){
+
+  $(".gallery ul").animate({
+    "left":-300*idx+"px"
+  },300);
+  $(".g_item ul li").eq(idx-1).addClass("on").siblings().removeClass("on");
+  idx++;
+  if(idx> $(".gallery ul li").length-3){
+    $(".gallery ul").animate({
+      "left":0
+    },0);
+    idx=0;   
+  }
+}
+
+$(".gallery , .g_item").hover(function(){
+  if(inter==true){
+    clearInterval(gall);
+    inter=false;
+  }
+},function(){
+  if(inter==false){
+    gall  = setInterval(galleryFun, 2000);
+    inter=true;
+  }
+});
+
+$(".g_item ul li").on('click',function(){
+  $(this).addClass("on").siblings().removeClass("on");
+  idx = $(this).index()+1;
+  $(".gallery ul").animate({
+    "left":-300*idx+"px"
+  },1000);
+});
