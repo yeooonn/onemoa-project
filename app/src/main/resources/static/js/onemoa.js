2885e0e867645bd8b7c0fde793659743c0e89ae4
YJung <rladuswjd0303@naver.com>
2022-10-24 17:30:13 +0900

    Design: 공모전페이지 추가 구현

--- app/src/main/resources/static/js/onemoa.js
+++ app/src/main/resources/static/js/onemoa.js
@@ -39,3 +39,156 @@ $(".g_item ul li").on('click',function(){
 });
 
 
+/* contest */
+
+function team(){
+  if ($('.person').css('display') == 'none'){
+    $('.person').show();
+    $('.contestIn').hide();
+    $('.contestdtype p').eq(1).css('color', '#000');
+    $('.contestdtype p').eq(1).css('border-bottom', '3px solid #000');
+    $('.contestdtype p').eq(1).css('padding-bottom', '7px');
+    $('.contestdtype p').eq(0).css('color', '#a5a5a5');
+    $('.contestdtype p').eq(0).css('border-bottom', 'none');
+  } else{
+    $('.person').hide();
+    $('.contestIn').show();
+    $('.contestdtype p').eq(0).css('color', '#000');
+    $('.contestdtype p').eq(0).css('border-bottom', '3px solid #000');
+    $('.contestdtype p').eq(0).css('padding-bottom', '7px');
+    $('.contestdtype p').eq(1).css('color', '#a5a5a5');
+    $('.contestdtype p').eq(1).css('border-bottom', 'none');
+  }
+  }
+
+function dis2(){
+  if ($('.modal3').css('display') == 'none'){
+    $('.modal3').show();
+    $('.modal2').hide();
+  } else{
+    $('.modal3').hide();
+    $('.modal2').hide();
+  }
+}
+function clo2(){
+  if ($('.modal3').css('display') == 'show'){
+    $('.modal3').hide();
+    $('.modal2').show();
+  } else{
+    $('.modal3').hide();
+    $('.modal2').show();
+  }
+}
+
+function dis3(){
+  if ($('.modal4').css('display') == 'none'){
+    $('.modal3').show();
+    $('.modal4').show();
+  } else{
+    $('.modal4').hide();
+  }
+}
+function clo3(){
+  if ($('.modal4').css('display') == 'show'){
+    $('.modal4').hide();
+    $('.modal3').show();
+  } else{
+    $('.modal4').hide();
+    $('.modal3').show();
+  }
+}
+
+function dis4(){
+  if ($('.modal5').css('display') == 'none'){
+    $('.modal2').show();
+    $('.modal5').show();
+  } else{
+    $('.modal2').show();
+    $('.modal5').hide();
+  }
+}
+function clo4(){
+  if ($('.modal5').css('display') == 'show'){
+    $('.modal5').hide();
+    $('.modal2').show();
+  } else{
+    $('.modal5').hide();
+    $('.modal2').show();
+  }
+}
+
+function dis5(){
+  if ($('.modal6').css('display') == 'none'){
+    $('.modal5').show();
+    $('.modal6').show();
+  } else{
+    $('.modal5').show();
+    $('.modal6').hide();
+    $('.modal7').hide();
+  }
+}
+function clo5(){
+  if ($('.modal6').css('display') == 'show'){
+    $('.modal6').hide();
+    $('.modal5').show();
+  } else{
+    $('.modal6').hide();
+    $('.modal5').show();
+  }
+}
+
+function dis6(){
+  if ($('.modal7').css('display') == 'none'){
+    $('.modal6').show();
+    $('.modal7').show();
+  } else{
+    $('.modal7').hide();
+  }
+}
+function clo6(){
+  if ($('.modal7').css('display') == 'show'){
+    $('.modal7').hide();
+    $('.modal6').show();
+  } else{
+    $('.modal7').hide();
+    $('.modal6').show();
+  }
+}
+
+function dis7(){
+  if ($('.modal8').css('display') == 'none'){
+    $('.modal5').hide();
+    $('.modal8').show();
+  } else{
+    $('.modal8').hide();
+  }
+}
+function clo7(){
+  if ($('.modal8').css('display') == 'show'){
+    $('.modal8').hide();
+    $('.modal5').show();
+  } else{
+    $('.modal8').hide();
+    $('.modal5').show();
+  }
+}
+
+function dis8(){
+  if ($('.modal9').css('display') == 'none'){
+    $('.modal8').show();
+    $('.modal9').show();
+  } else{
+    $('.modal8').hide();
+    $('.modal9').hide();
+    $('.modal5').show();
+  }
+}
+function clo8(){
+  if ($('.modal9').css('display') == 'show'){
+    $('.modal9').hide();
+    $('.modal8').show();
+  } else{
+    $('.modal9').hide();
+    $('.modal8').show();
+  }
+}
