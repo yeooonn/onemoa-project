49f195b7a645c5aad1bb570e2ca0cf4762e54e73
jyung17 <iotaxixi@gmail.com>
2022-10-31 15:31:53 +0900

    Feat: 회원가입 이메일 인증 구현

--- app/src/main/java/com/bitcamp/onemoaproject/controller/AuthController.java
+++ app/src/main/java/com/bitcamp/onemoaproject/controller/AuthController.java
@@ -5,35 +5,29 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.bitcamp.onemoaproject.service.MailService;
 import com.bitcamp.onemoaproject.service.MemberService;
+import com.bitcamp.onemoaproject.vo.Mail;
 import com.bitcamp.onemoaproject.vo.Member;
 
 @Controller
 public class AuthController {
+
+  @Autowired
   MemberService memberService;
 
-  public AuthController(MemberService memberService) {
-    this.memberService = memberService;
-  }
-
-  @GetMapping("index")
-  public String form(@CookieValue(name = "email", defaultValue = "") String email, Model model) {
-    System.out.println(email);
-    model.addAttribute("email", email);
-    System.out.println(model);
-    return "index";
-  }
+  @Autowired
+  MailService mailService;
 
   @ResponseBody
   @PostMapping("login")
   public String login(String email, String password, String saveEmail, HttpServletRequest request,
-      Model model, HttpServletResponse response, HttpSession session) throws Exception {
+      HttpServletResponse response, HttpSession session) throws Exception {
     Member member = memberService.get(email, password);
 
     if (member != null) {
@@ -49,8 +43,6 @@ public class AuthController {
 
     response.addCookie(cookie);
 
-    model.addAttribute("member", member);
-
     if (member == null) {
       return "false";
     }
@@ -60,14 +52,13 @@ public class AuthController {
   @GetMapping("logout")
   public String logout(HttpSession session) throws Exception {
     session.invalidate(); // 현재 세션을 무효화시킨다.
-    return "redirect:index";
+    return "redirect:/";
   }
 
   @ResponseBody
   @PostMapping("/nicknamecheck")
   public String nickNameCheck(String nickname) throws Exception {
     Member member = memberService.getNickName(nickname);
-    System.out.println(member);
     if (member != null) {
       return "false";
     }
@@ -87,25 +78,11 @@ public class AuthController {
     Random random = new Random();
     int checkNum = random.nextInt(888888) + 111111;
 
-    /* 이메일 보내기 */
-    String setFrom = "자신의 이메일을 입력해주세요";
-    String toMail = email;
-    String title = "회원가입 인증 이메일 입니다.";
-    String content = "홈페이지를 방문해주셔서 감사합니다." + "<br><br>" + "인증 번호는 " + checkNum + "입니다." + "<br>"
-        + "해당 인증번호를 인증번호 확인란에 기입하여 주세요.";
-    System.out.println(toMail);
-    //    try {
-    //      MimeMessage message = mailSender.createMimeMessage();
-    //      MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
-    //      helper.setFrom(setFrom);
-    //      helper.setTo(toMail);
-    //      helper.setSubject(title);
-    //      helper.setText(content,true);
-    //      mailSender.send(message);
-    //    }catch(Exception e) {
-    //      e.printStackTrace();
-    //    }
-    System.out.println(Integer.toString(checkNum));
+    Mail mail = new Mail();
+    mail.setAddress(email);
+    mail.setTitle("[onemoa] 이메일 계정 인증");
+    mail.setCheckNum(checkNum);
+    mailService.sendTemplateMessage(mail);
     return Integer.toString(checkNum);
   }
 }
---
+++ app/src/main/java/com/bitcamp/onemoaproject/service/MailService.java
@@ -0,0 +1,50 @@
+package com.bitcamp.onemoaproject.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import com.bitcamp.onemoaproject.vo.Mail;
+
+@Service
+public class MailService {
+
+  @Autowired
+  private JavaMailSender emailSender;
+
+  @Autowired
+  private SpringTemplateEngine templateEngine;
+
+  public void sendTemplateMessage(Mail mail) throws MessagingException {
+    MimeMessage message = emailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+    //메일 제목 설정
+    helper.setSubject(mail.getTitle());
+
+    //수신자 설정
+    helper.setTo(mail.getAddress());
+
+    //참조자 설정
+    //helper.setCc(mail.getCcAddress());
+
+    String email = mail.getAddress();
+    String checkNum = String.valueOf(mail.getCheckNum());
+
+    //템플릿에 전달할 데이터 설정
+    Context context = new Context();
+    context.setVariable("email", email);
+    context.setVariable("checkNum", checkNum);
+
+    //메일 내용 설정 : 템플릿 프로세스
+    String html = templateEngine.process("emailCode", context);
+    helper.setText(html, true);
+
+    //메일 보내기
+    emailSender.send(message);
+  }
+}
+
