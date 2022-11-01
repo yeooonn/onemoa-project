585706a934a3356c3a1c5dd51af73f01ff9ca8b6
ddongpuri <fnffnfkffk1112@gmail.com>
2022-11-01 09:52:55 +0900

    product branch 생성

---
+++ app/src/main/java/com/bitcamp/onemoaproject/vo/product/ProductCategory.java
@@ -0,0 +1,25 @@
+package com.bitcamp.onemoaproject.vo.product;
+
+public class ProductCategory {
+
+    private String code;
+    private int tier;
+    private String name;
+    private String parent;
+
+    public String getCode() { return code;}
+
+    public void setCode(String code) {this.code = code;}
+
+    public int getTier() {return tier;}
+
+    public void setTier(int tier) {this.tier = tier;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getParent() {return parent;}
+
+    public void setParent(String parent) {this.parent = parent;}
+}
