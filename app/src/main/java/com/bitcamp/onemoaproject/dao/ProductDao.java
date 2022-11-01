9c775e087fb0bd702ba75d63c48deddcc4f8df93
ddongpuri <fnffnfkffk1112@gmail.com>
2022-11-01 14:15:53 +0900

    product 관련 클래스 파일  추가

---
+++ app/src/main/java/com/bitcamp/onemoaproject/dao/ProductDao.java
@@ -0,0 +1,50 @@
+package com.bitcamp.onemoaproject.dao;
+
+import java.util.List;
+
+import com.bitcamp.onemoaproject.vo.product.Product;
+import org.apache.ibatis.annotations.Mapper;
+import com.bitcamp.onemoaproject.vo.product.Product;
+
+@Mapper
+public interface ProductDao {
+
+  int insert(Product product);
+
+  Product findByNo(int no);
+
+  int update(Product product);
+
+  int delete(int no);
+
+  //  int deleteByMember(int memberNo);
+
+  List<Product> findAll();
+
+  int insertFiles(Product product);
+
+  //  AttachedFile findFileByNo(int fileNo);
+  //
+  //  List<AttachedFile> findFilesByProduct(int ProductNo);
+  //
+  //  int deleteFile(int fileNo);
+  //
+  //  int deleteFiles(int ProductNo);
+  //
+  //  int deleteFilesByMemberProducts(int memberNo);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
