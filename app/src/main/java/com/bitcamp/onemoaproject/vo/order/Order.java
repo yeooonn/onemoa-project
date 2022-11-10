e6b46e044ea823bcd8989518c1a4817c44da2c6c
ddongpuri <fnffnfkffk1112@gmail.com>
2022-11-10 14:05:42 +0900

    .

--- app/src/main/java/com/bitcamp/onemoaproject/vo/order/Order.java
+++ app/src/main/java/com/bitcamp/onemoaproject/vo/order/Order.java
@@ -1,9 +1,15 @@
 package com.bitcamp.onemoaproject.vo.order;
 
 import com.bitcamp.onemoaproject.vo.Member;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.sql.Date;
 
+@Getter
+@Setter
+@ToString
 public class Order {
 
     private int no;
