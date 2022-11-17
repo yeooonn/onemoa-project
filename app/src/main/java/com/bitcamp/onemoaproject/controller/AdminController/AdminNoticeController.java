1563d30089ec306ad98c64a128c11763b03e09d4
jyung17 <iotaxixi@gmail.com>
2022-11-17 23:06:01 +0900

    Refactor: 공지사항 관리자페이지 분리

--- app/src/main/java/com/bitcamp/onemoaproject/controller/AdminController/AdminNoticeController.java
+++ app/src/main/java/com/bitcamp/onemoaproject/controller/AdminController/AdminNoticeController.java
@@ -1,5 +1,8 @@
-package com.bitcamp.onemoaproject.controller;
+package com.bitcamp.onemoaproject.controller.AdminController;
 
+
+import com.bitcamp.onemoaproject.service.NoticeService;
+import com.bitcamp.onemoaproject.vo.Notice;
 import com.bitcamp.onemoaproject.vo.paging.Criteria;
 import com.bitcamp.onemoaproject.vo.paging.PageMaker;
 import java.util.HashMap;
@@ -7,28 +10,36 @@ import java.util.List;
 import java.util.Map;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
-import org.springframework.boot.Banner.Mode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.bitcamp.onemoaproject.service.NoticeService;
-import com.bitcamp.onemoaproject.vo.Notice;
 
-// CRUD 요청을 처리하는 페이지 컨트롤러들을 한 개의 클래스로 합친다. 
-@Controller // 페이지 컨트롤러에 붙이는 애노테이션 
-@RequestMapping("/notice/")
-public class NoticeController {
-
+@Controller
+@RequestMapping("admin/notice")
+public class AdminNoticeController {
+  
+  @Autowired
   ServletContext sc;
+  
+  @Autowired
   NoticeService noticeService;
-
-  public NoticeController(NoticeService noticeService, ServletContext sc) {
-    System.out.println("NoticeController() 호출됨!");
-    this.noticeService = noticeService;
-    this.sc = sc;
+  
+  // InternalResourceViewResolver 사용 후:
+  @GetMapping("form")
+  public void form() throws Exception {
+  }
+  
+  @PostMapping("add")
+  public String add(
+      Notice notice,
+      HttpSession session) throws Exception {
+    
+    noticeService.add(notice);
+    return "redirect:list";
   }
   
   // 페이징
@@ -39,7 +50,7 @@ public class NoticeController {
     pageMaker.setCri(cri);
     pageMaker.setTotalCount(noticeService.listCount());
     List<Map<String, Object>> notices = noticeService.list(cri);
-  
+    
     model.addAttribute("notices", notices);
     model.addAttribute("pageMaker", pageMaker);
     System.out.println("pageMaker = " + pageMaker);
@@ -47,21 +58,43 @@ public class NoticeController {
   
   @GetMapping("detail")
   public Map detail(int no) throws Exception {
-
+    
     Notice notice = noticeService.get(no);
-
+    
     //    if (notice == null) {
     //      throw new Exception("해당 번호의 게시글이 없습니다!");
     //    }
-
-    Map map = new HashMap(); 
+    
+    Map map = new HashMap();
     map.put("notice", notice);
     return map;
   }
+  
+  @PostMapping("update")
+  public String update(
+      Notice notice,
+      HttpSession session) throws Exception {
+    
+    //    checkOwner(board.getNo(), session);
+    
+    if (!noticeService.update(notice)) {
+      throw new Exception("게시글을 변경할 수 없습니다.");
+    }
+    
+    return "redirect:list";
+  }
+  
+  @GetMapping("delete")
+  public String delete(
+      @RequestParam("no") int no,
+      HttpSession session)
+      throws Exception {
+    
+    //    checkOwner(no, session);
+    if (!noticeService.delete(no)) {
+      throw new Exception("게시글을 삭제할 수 없습니다!");
+    }
+    
+    return "redirect:list";
+  }
 }
-
-
-
-
-
-
