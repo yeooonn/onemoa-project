87ccf56b62e8fb76a59c77907ec15a16cd631932
YJung <rladuswjd0303@naver.com>
2022-11-22 18:15:53 +0900

    Design: message css 수정

--- app/src/main/java/com/bitcamp/onemoaproject/controller/MessageController.java
+++ app/src/main/java/com/bitcamp/onemoaproject/controller/MessageController.java
@@ -1,8 +1,6 @@
 package com.bitcamp.onemoaproject.controller;
 
 
-import com.bitcamp.onemoaproject.service.MessageService;
-import com.bitcamp.onemoaproject.vo.Member;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.bitcamp.onemoaproject.service.MemberService;
+import com.bitcamp.onemoaproject.service.MessageService;
+import com.bitcamp.onemoaproject.vo.Member;
 
 @Controller
 @RequestMapping("/message/")
@@ -17,7 +18,9 @@ public class MessageController {
 
   @Autowired
   MessageService messageService;
-  
+  @Autowired
+  MemberService memberService;
+
   // 메시지 미리보기 조회 (사이드바)
   @GetMapping("messaget")
   public String messageForm(HttpSession session, Model model) throws Exception {
@@ -28,7 +31,7 @@ public class MessageController {
     System.out.println("messageService.list() = " + messageService.list(sender));
     return "message/messaget";
   }
-  
+
   // 메시지 상세보기
   @GetMapping("messageDetail")
   public String messageList(HttpSession session, Model model, int no) throws Exception {
@@ -36,11 +39,12 @@ public class MessageController {
     Member loginMember = (Member) session.getAttribute("loginMember");
     int sender = loginMember.getNo();
     model.addAttribute("member", loginMember);
+    model.addAttribute("receiverMember", memberService.get(no));
     model.addAttribute("messages", messageService.list(sender));
     model.addAttribute("details", messageService.listNo(sender, no));
     return "message/messageDetail";
   }
-  
+
   // 메시지 저장
   @PostMapping("sendContent")
   public String messageAdd(int sender, int receiver, String contest) throws Exception {
